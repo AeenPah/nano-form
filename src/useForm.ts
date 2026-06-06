2e5ac5feaada5f9201043b37c6525de37fb4ceb5
Aien Pahlevan <aienpahlevanhim@gmail.com>
2026-06-06 21:27:49 +0330

    feat: Update use form hook to support asynchronous default values

--- src/useForm.ts
+++ src/useForm.ts
@@ -1,4 +1,4 @@
-import { ChangeEvent, FormEvent, useRef, useState } from "react";
+import { ChangeEvent, SubmitEvent, useEffect, useRef, useState } from "react";
 
 type TErrors<T> = Partial<Record<keyof T, string>>;
 
@@ -7,29 +7,60 @@ function useForm<T extends Record<string, string>>(
   validate?: (values: T) => TErrors<T>,
 ) {
   /* -------------------------------------------------------------------------- */
-  /*                                 React Hooks                                */
+  /*                                    Refs                                    */
   /* -------------------------------------------------------------------------- */
 
+  const lastInitialValuesRef = useRef<T>(initialValues);
   const valuesRef = useRef<T>({ ...initialValues });
+
   const formRef = useRef<HTMLFormElement>(null);
 
+  /* -------------------------------------------------------------------------- */
+  /*                                   States                                   */
+  /* -------------------------------------------------------------------------- */
+
   const [errors, setErrors] = useState<TErrors<T>>();
+  const [values, setValues] = useState<T>(initialValues);
+
   const [isSubmitting, setIsSubmitting] = useState(false);
+
+  /* -------------------------------------------------------------------------- */
+  /*                                   Effects                                  */
+  /* -------------------------------------------------------------------------- */
+
+  useEffect(() => {
+    const updateValues = () => {
+      lastInitialValuesRef.current = initialValues;
+      valuesRef.current = initialValues;
+      setValues(initialValues);
+    };
+
+    if (
+      JSON.stringify(lastInitialValuesRef.current) !==
+      JSON.stringify(initialValues)
+    ) {
+      updateValues();
+    }
+  }, [initialValues]);
 
   /* -------------------------------------------------------------------------- */
   /*                                  Functions                                 */
   /* -------------------------------------------------------------------------- */
 
   function handleChange(
-    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
   ) {
-    const { name, value } = e.target;
+    const { name, value } = event.target;
+    (valuesRef.current as Record<string, string>)[name] = value;
+  }
+
+  function handleSelectOption(name: string, value: string) {
     (valuesRef.current as Record<string, string>)[name] = value;
   }
 
   function handleSubmit(onSubmit: (values: T) => Promise<void> | void) {
-    return async (e: FormEvent) => {
-      e.preventDefault();
+    return async (event: SubmitEvent) => {
+      event.preventDefault();
 
       const currentValues = { ...valuesRef.current };
 
@@ -46,7 +77,6 @@ function useForm<T extends Record<string, string>>(
         await onSubmit(currentValues);
       } finally {
         setIsSubmitting(false);
-        reset();
       }
     };
   }
@@ -58,12 +88,14 @@ function useForm<T extends Record<string, string>>(
   }
 
   return {
+    values,
     formRef,
-    values: initialValues,
     errors,
     isSubmitting,
     handleChange,
     handleSubmit,
+    reset,
+    handleSelectOption,
   };
 }
 
